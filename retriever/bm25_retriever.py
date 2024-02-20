b48f0a5259f9db7f07b594b4437514efde68f01d
Tianze Shou <shoutianze@outlook.com>
2024-02-20 16:41:45 -0500

    Revise retriever with the new document corpus format

--- retriever/bm25_retriever.py
+++ retriever/bm25_retriever.py
@@ -1,4 +1,5 @@
 import os
+from collections import deque
 from rank_bm25 import BM25Okapi
 
 
@@ -9,16 +10,41 @@ class BM25Retriever:
         file_names = os.listdir(knowledge_source_path)
         for file_name in file_names:
             with open(os.path.join(knowledge_source_path, file_name), "r") as file:
-                self.corpus.append(file.read().lower())
+                doc = file.read().split("<sep>")
+                self.corpus.extend(doc)
 
-        self.bm25 = BM25Okapi(self.corpus)
+        self.corpus, tokenized_corpus = _process_corpus(self.corpus)
+        self.bm25 = BM25Okapi(tokenized_corpus)
 
-    def retrieve(self, question: str):
+    def retrieve(self, question: str, top_n: int = 5):
         tokenized_question = question.lower().split()
-        return self.bm25.get_top_n(question, self.corpus)
+        return self.bm25.get_top_n(tokenized_question, self.corpus)
 
-r = BM25Retriever()
-docs = r.retrieve("what masters program does LTI offer")
-for doc in docs:
-    print(doc)
-    print()
+
+def _process_corpus(corpus: list[str]) -> tuple[list[str], list[list[str]]]:
+    punctuations = "`~!@#$%^&*()_+[]\\;',./{}|:\"<>?"
+
+    org_corpus_set = set()
+    org_corpus = deque()
+    tokenized_corpus = deque()
+    for doc in corpus:
+        if doc in org_corpus_set:
+            continue
+
+        d = doc.lower()
+        for punc in punctuations:
+            d = d.replace(punc, "")
+        tokenized_doc = d.split()
+        tokenized_corpus.append(tokenized_doc)
+        org_corpus.append(doc)
+        org_corpus_set.add(doc)
+
+    return list(org_corpus), list(tokenized_corpus)
+
+
+if __name__ == "__main__":
+    r = BM25Retriever()
+    docs = r.retrieve("What is Professor Yonatan Bisk's research interests")
+    for doc in docs:
+        print(doc)
+        print()
