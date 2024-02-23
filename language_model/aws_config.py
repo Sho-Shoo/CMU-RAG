7f1904a4a31bc360e271ebd08db942240ff169c4
Tianze Shou <shoutianze@outlook.com>
2024-02-23 13:50:39 -0500

    Add writeup for AWSConfig class

--- language_model/aws_config.py
+++ language_model/aws_config.py
@@ -1,6 +1,7 @@
 """
+IMPORTANT
 Do not push changes to this file onto the remote repo. All setting here will be local to you. After editing this file,
-please use `git update-index --assume-changed language_model/aws_config.py` to not track these changes. If you want to
+please use `git update-index --assume-unchanged language_model/aws_config.py` to not track these changes. If you want to
 update the attributes trakced in this file, use `git update-index --no-assume-unchanged language_model/aws_config.py`
 to enable tracking.
 """
