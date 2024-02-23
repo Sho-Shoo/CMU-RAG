32150382d244f70a13a3d525118c0e4a505bad44
Tianze Shou <shoutianze@outlook.com>
2024-02-23 13:38:40 -0500

    Add writeup for AWSConfig class

--- language_model/aws_config.py
+++ language_model/aws_config.py
@@ -1,3 +1,11 @@
+"""
+Do not push changes to this file onto the remote repo. All setting here will be local to you. After editing this file,
+please use `git update-index --assume-changed language_model/aws_config.py` to not track these changes. If you want to
+update the attributes trakced in this file, use `git update-index --no-assume-unchanged language_model/aws_config.py`
+to enable tracking.
+"""
+
+
 class AWSConfig:
     SAGEMAKER_ENDPOINT_NAME = "todo"
     SAGEMAKER_ARN_ROLE = "todo"
