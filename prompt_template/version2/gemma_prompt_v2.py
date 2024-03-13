a1051375a477d020cfe001fa20f445fa26666c00
Jing Zhang <ankie.zj@gmail.com>
2024-03-13 13:32:40 -0400

    Fix error in prompt and update results

--- prompt_template/version2/gemma_prompt_v2.py
+++ prompt_template/version2/gemma_prompt_v2.py
@@ -2,7 +2,7 @@
 ZERO_TEMPLATE_V2 = \
 """
 <bos><start_of_turn>user
-You are a question-answering assistant. You will be provided with a set of DOCUMENTS about Carnegie Mellon University (CMU) and the Language Technology Institute (LTI), marked by 'CONTEXT START' and 'CONTEXT END'. These DOCUMENTS are composed of multiple subdocuments, each separated by a newline character ('\n'). Your task is to provide a short and simple answer to the QUESTION based on the DOCUMENTS, considering only the most relevant subdocument.Begin your answer with a concise statement that directly addresses the question. If the DOCUMENTS do not contain the necessary information, please answer "I don't know". Keep the answer brief and to the point. <end_of_turn>
+You are a question-answering assistant. You will be provided with a set of DOCUMENTS about Carnegie Mellon University (CMU) and the Language Technology Institute (LTI), marked by 'CONTEXT START' and 'CONTEXT END'. These DOCUMENTS are composed of multiple subdocuments, each separated by a newline character ('\n'). Your task is to provide a short and simple answer to the QUESTION based on the DOCUMENTS, considering only the most relevant subdocument.Begin your answer with a concise statement that directly addresses the question. If the DOCUMENTS do not contain the necessary information, please answer "I don't know". Keep the answer brief and to the point.
 
 CONTEXT START
 {context}
