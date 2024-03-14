e7960010688a78b020f74894118a249b2908f6ab
Tianze Shou <shoutianze@outlook.com>
2024-03-14 13:49:00 -0400

    Update in-context examples

--- language_model/utils.py
+++ language_model/utils.py
@@ -1,18 +1,30 @@
-_IN_COMTEXT_EXAMPLES = \
-"""
-Q: Where is Professor Graham Neubig's office?
-A: Professor Graham Neubig's office is Room 5409 of the Gates & Hillman Centers.
-
-Q: Who is teaching 11711 in Fall 2023?
-A: Professor Robert Frederking and Daniel Fried.
-
-Q: Who are the co-authors of the paper "Generating Images with Multimodal Language Models" published in Neural Information Processing Systems in 2023, and what is the main proposal of this research?
-A: The co-authors of the paper are Jing Yu Koh, Daniel Fried, and R. Salakhutdinov. The main proposal of this research is a method to fuse frozen text-only large language models (LLMs) with pre-trained image encoder and decoder models, by mapping between their embedding spaces, and exhibits a wider range of capabilities compared to prior multimodal language models.
-
-Q: What is Chute Flagger?
-A: Chute Flagger is team member who provides a signal for buggy drivers to know when to start the right-hand turn from Schenley Drive onto Frew Street.
-"""
-
-
 def get_in_context_example() -> str:
-    return _IN_COMTEXT_EXAMPLES
+    questions, answers = [], []
+
+    with open("data/train/questions.txt", "r") as f:
+        while True:
+            line = f.readline().strip()
+            if not line: break
+            questions.append(line)
+
+    with open("data/train/reference_answers.txt", "r") as f:
+        while True:
+            line = f.readline().strip()
+            if not line: break
+            answers.append(line)
+
+    if len(questions) != len(answers):
+        raise RuntimeError("In context QA pair count mismatch.")
+
+    examples = []
+    for q, a in zip(questions, answers):
+        example = f"Q: {q}\nA: {a}"
+        examples.append(example)
+
+    examples = "\n\n".join(examples)
+
+    return examples.strip()
+
+
+if __name__ == "__main__":
+    print(get_in_context_example())
