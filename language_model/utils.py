55e905451abb0b541b601f66af75bd468b09b53b
Jing Zhang <ankie.zj@gmail.com>
2024-03-14 16:43:18 -0400

    Add the function to create in-context learning examples for Gemma.

--- language_model/utils.py
+++ language_model/utils.py
@@ -1,4 +1,4 @@
-def get_in_context_example() -> str:
+def get_in_context_example(is_gemma = False) -> str:
     questions, answers = [], []
 
     with open("data/train/questions.txt", "r") as f:
@@ -17,9 +17,14 @@ def get_in_context_example() -> str:
         raise RuntimeError("In context QA pair count mismatch.")
 
     examples = []
-    for q, a in zip(questions, answers):
-        example = f"Q: {q}\nA: {a}"
-        examples.append(example)
+    if is_gemma:
+        for q, a in zip(questions, answers):
+            example = f"<start_of_turn>user\nQ: {q}<end_of_turn>\n<start_of_turn>model\nA: {a}<end_of_turn>"
+            examples.append(example)
+    else:
+        for q, a in zip(questions, answers):
+            example = f"Q: {q}\nA: {a}"
+            examples.append(example)
 
     examples = "\n\n".join(examples)
 
@@ -28,3 +33,4 @@ def get_in_context_example() -> str:
 
 if __name__ == "__main__":
     print(get_in_context_example())
+    print(get_in_context_example(is_gemma=True))
--- prompt_template/version1/gemma_prompt_v1.py
+++ prompt_template/version1/gemma_prompt_v1.py
@@ -16,19 +16,10 @@ ANSWER:<end_of_turn>
 FEW_TEMPLATE_V1 = \
 """
 <bos><start_of_turn>user
-You are a question-answering assistant who provides a short answer to a QUESTION based on your knowledge or on the CONTEXT about Carnegie Mellon University (CMU) and Language Technology Institute (LTI). If the CONTEXT does not contain necessary information, please answer 'I don't know'. Please keep the answer short and simple. Here are a few examples:<end_of_turn>
+You are a question-answering assistant who provides a short answer to a QUESTION based on your knowledge or on the CONTEXT about Carnegie Mellon University (CMU) and Language Technology Institute (LTI). If the CONTEXT does not contain necessary information, please answer 'I don't know'. Please keep the answer short and simple. Here are a few examples:
 
-<start_of_turn>user
-Question: When is 2024 Spring Carnival?<end_of_turn>
-<start_of_turn>model
-Answer: The 2024 Spring Carnival is from April 11 to April 14.<end_of_turn>
+{in-context learning}
 
-<start_of_turn>user
-Question: When was Carnegie Mellon University founded?<end_of_turn>
-<start_of_turn>model
-Answer: The Carnegie Mellon University was founded in 1900.<end_of_turn>
-
-<start_of_turn>user
 CONTEXT:
 {context}
 
